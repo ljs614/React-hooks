9f986ab1cda18a8dd781c9c99d88b150f93188f0
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-04-14 17:54:09 +0900

    userState

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from "react";
+const App = () => {
+    const [item, setItem ] = useState(1);
+    const incrementItem = () => setItem(item + 1);
+    const decrementItem = () => setItem(item - 1);
+    return (
+      <div>
+        <h1>Hello {item}</h1>
+        <button onClick={incrementItem}>Increse</button>
+        <button onClick={decrementItem}>Decrese</button>
+      </div>
+    );
 }
 
 export default App;
