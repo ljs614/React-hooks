c8954efc14bb8468e6acbb6301cc7cd6b17cdb0e
ljs614 <sjl0614@naver.com>
2020-04-15 23:05:54 +0900

    useInput

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
+import useInput from "./useInput";
+
+
+
 const App = () => {
     const [item, setItem ] = useState(1);
     const incrementItem = () => setItem(item + 1);
     const decrementItem = () => setItem(item - 1);
+
+    const maxLen = (value) => value.length < 10;
+    
+    const name = useInput("Mr.", maxLen);
+
     return (
-      <div>
-        <h1>Hello {item}</h1>
-        <button onClick={incrementItem}>Increse</button>
-        <button onClick={decrementItem}>Decrese</button>
-      </div>
+      <>
+        <div>
+          <h1>Hello {item}</h1>
+          <button onClick={incrementItem}>Increse</button>
+          <button onClick={decrementItem}>Decrese</button>
+        </div>
+        <div>
+          <h1>useInput</h1>
+          <input placeholder="Name" {...name} />
+        </div>
+      </>
     );
 }
 
