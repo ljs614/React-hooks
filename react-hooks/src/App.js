d2b2ef0fe1b5e6e855b9a5fbd80af19bfeef7220
ljs614 <sjl0614@naver.com>
2020-04-16 23:17:10 +0900

    useTabs

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
-import useInput from "./useInput";
+import useTabs from "./useTabs"
+
+const content = [
+  {
+      tab: "Section 1",
+      content: "I'm the content of the ection 1"
+  },
+  {
+      tab: "Section 2",
+      content: "I'm the content of the ection 2"
+  }
+];
 
 
 
 const App = () => {
-    const [item, setItem ] = useState(1);
-    const incrementItem = () => setItem(item + 1);
-    const decrementItem = () => setItem(item - 1);
-
-    const maxLen = (value) => value.length < 10;
-    
-    const name = useInput("Mr.", maxLen);
-
+  const {currentItem, changeItem} = useTabs(0, content);
     return (
-      <>
-        <div>
-          <h1>Hello {item}</h1>
-          <button onClick={incrementItem}>Increse</button>
-          <button onClick={decrementItem}>Decrese</button>
-        </div>
-        <div>
-          <h1>useInput</h1>
-          <input placeholder="Name" {...name} />
-        </div>
-      </>
+      <div>
+        {content.map((section, index) => (
+          <button onClick={() => changeItem(index)}>{section.tab}</button>
+        ))}
+        <div>{currentItem.content}</div>
+      </div>
     );
 }
 
