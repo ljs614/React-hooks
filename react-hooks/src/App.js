ab6858ec8841f7ca8ae491f0bd20559bb1df3006
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-04-17 10:52:30 +0900

    useEffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,29 +1,17 @@
-import React, { useState } from "react";
-import useTabs from "./useTabs"
-
-const content = [
-  {
-      tab: "Section 1",
-      content: "I'm the content of the ection 1"
-  },
-  {
-      tab: "Section 2",
-      content: "I'm the content of the ection 2"
-  }
-];
-
-
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const {currentItem, changeItem} = useTabs(0, content);
-    return (
-      <div>
-        {content.map((section, index) => (
-          <button onClick={() => changeItem(index)}>{section.tab}</button>
-        ))}
-        <div>{currentItem.content}</div>
-      </div>
-    );
+  const sayHello = () => console.log("Hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setANumber] = useState(0);
+  useEffect(sayHello, [number]);
+  return (
+    <div>
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
 }
 
 export default App;
