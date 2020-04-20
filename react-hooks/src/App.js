642edff212eda3de11dffc345acf19dc7f60adf6
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-04-20 17:09:25 +0900

    useFullscreen

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,20 @@
 import React, { useRef, useEffect } from "react";
-import useScroll from "./useScroll";
-
-
+import useFullscreen from "./useFullscreen";
 
 const App = () => {
-  const { y } = useScroll();
+  const onFullS = isFull => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull} = useFullscreen(onFullS);
   return (
-    <div style={{height: "1000vh"}}>
-      <h1 style={{position: "fixed", color: y> 100 ? "red" : "blue"}}>Hello</h1>
+    <div>
+      <img 
+        ref={element}
+        src="https://images.unsplash.com/photo-1586521995659-4204fe01d104?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80" 
+        style={{width: "800px"}}
+        onClick={exitFull}
+      />
+      <button onClick={triggerFull}>Full Screen</button>
     </div>
   );
 }
