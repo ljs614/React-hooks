42125c1bf81accd05857be9d691e2ce5105e21d4
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-04-21 14:18:04 +0900

    useNotification

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,20 +1,11 @@
 import React, { useRef, useEffect } from "react";
-import useFullscreen from "./useFullscreen";
+import useNotification from "./useNotification";
 
 const App = () => {
-  const onFullS = isFull => {
-    console.log(isFull ? "We are full" : "We are small");
-  };
-  const { element, triggerFull, exitFull} = useFullscreen(onFullS);
+  const triggerNotif = useNotification("Can I steal your kimchi", {body:"I love kimchi"});
   return (
     <div>
-      <img 
-        ref={element}
-        src="https://images.unsplash.com/photo-1586521995659-4204fe01d104?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80" 
-        style={{width: "800px"}}
-        onClick={exitFull}
-      />
-      <button onClick={triggerFull}>Full Screen</button>
+      <button onClick={triggerNotif}>Hello</button>
     </div>
   );
 }
